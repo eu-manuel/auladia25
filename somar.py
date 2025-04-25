da8bf48994eec62b452175e61e38f4724f0b4045
Emanuel junqueira <emanuel.junqueira@gmail.com>
2025-04-25 20:15:05 -0300

    adição do arquivo somar

---
+++ somar.py
@@ -0,0 +1,10 @@
+def somar():
+    a = int(input("digite o primeiro numero da soma: "))
+    b = int(input("digite o segundo numero da soma: "))
+    print(a+b)
+
+def divisao():
+    a = int(input("digite o primeiro numero da soma: "))
+    b = int(input("digite o segundo numero da soma: "))
+
+    print(a/b)
